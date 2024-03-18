9a081a0f8247dc2389442949ebf02d42cde25308
Benjamin Herzig <benjamin.herzig@volkswage.de>
2024-03-18 16:32:50 +0100

    add rose pine theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,10 +1,12 @@
 return {
   "AstroNvim/astrocommunity",
   -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
 }
--- plugins/core.lua
+++ plugins/core.lua
@@ -1,3 +1,9 @@
 return {
   {"imsnif/kdl.vim"},
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
+    opts = {},
+  }
 }
